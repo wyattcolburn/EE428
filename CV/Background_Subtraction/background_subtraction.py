95f3d8e5db2c2405a288116942aeef3d2f07fea9
wyattcolburn <wyattdcolburn@gmail.com>
2024-10-03 16:41:27 -0700

    finished i think

--- CV/Background_Subtraction/background_subtraction.py
+++ CV/Background_Subtraction/background_subtraction.py
@@ -4,11 +4,9 @@ import skimage as ski
 import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
 import os
 import imageio
-from skimage.measure import label, regionprops
-from skimage.color import label2rgb
+from skimage.measure import label
 
 def show_video(images,title):
     plt.title(title)
@@ -38,11 +36,11 @@ def main():
         image_list.append(gray_image)
     video = np.array(image_list)
     show_video(video, 'prof img')
-    #display_video(video)
 
     background_frame = np.mean(video, axis=0)  # not sure why axis 0
     plt.imshow(background_frame, cmap='gray')
     plt.axis('off')
+    plt.title('background frame')
     plt.show()
     
     """difference of first frame and background picture should just be the cars,
@@ -58,6 +56,7 @@ def main():
     
     plt.imshow(difference, cmap='gray')
     plt.axis('off')
+    plt.title('difference between first frame and background')
     plt.show()
     # otsu method splits an image into two classes: foreground and background. Want to maximize the variance between classes. Split greyscale image into a binary image, so white is 1 and black is 0. Thats where the threshold comes into affect, values below threshold are 0, above 1. 
 
@@ -65,6 +64,7 @@ def main():
     binary = difference >= threshold #  example from link below
     plt.imshow(binary, cmap='binary')
     plt.axis('off')
+    plt.title('binary mask')
     plt.show()
 #  https://scikit-image.org/docs/stable/api/skimage.filters.html#skimage.filters.threshold_otsu
         
@@ -80,29 +80,6 @@ def main():
 
     prof_mask = np.array(prof_list)
     show_video(prof_mask, 'Using Professor Func to Draw Boxes')
-def test():
-    
-    image_list = []
-    for filename in sorted(os.listdir('frames/')):
-        image = imageio.v2.imread(f'frames/{filename}')
-        gray_image = ski.color.rgb2gray(image) #this turns each pixel into a float 0-1
-        image_list.append(gray_image)
-    video = np.array(image_list)
-
-    background_frame = np.mean(video, axis=0)  # not sure why axis 0
-
-    prof_list = []
-    for filename in sorted(os.listdir('frames/')):
-        current_frame = imageio.v2.imread(f'frames/{filename}')
-        gray_image = ski.color.rgb2gray(current_frame)
-        difference = np.abs(gray_image - background_frame)
-        threshold = ski.filters.threshold_otsu(difference)
-        binary = difference >= threshold
-        frame = draw_bounding_boxes(current_frame, binary)
-        prof_list.append(frame)
-
-    prof_mask = np.array(prof_list)
-    show_video(prof_mask, 'Using Professor Func to Draw Boxes')
 
 if __name__ == "__main__":
-    test()
+    main()
